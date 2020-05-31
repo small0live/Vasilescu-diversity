b8fc7831f090821a0c9adfc2f82b28edee41a2b8
small0live <olivianewton@knights.ucf.edu>
2020-05-31 09:36:02 -0400

    regression tings

--- GHDiversityData/logistic_regression.R
+++ GHDiversityData/logistic_regression.R
@@ -1,123 +1,237 @@
 
 # Load Packages -----------------------------------------------------------
 
+library(data.table)
 library(DescTools)
 library(GGally)
+library(ggplot2)
 library(lattice)
 library(lme4)
-
-# Prepare Data ------------------------------------------------------------
-
-ks.sizes <- (kitchen.sink %>%
-               subset(gender != "None") %>%
-               mutate(sizeClass=
-                        ifelse(num_team < 11, "small", 
-                               ifelse(num_team > 10 & num_team < 30, "medium", 
-                                      "large"
-                               ))))
+library(tidyverse)
 
 
-wID <- sort(c(unique(ks.sizes$windows), 1))
-ks.sizes$window_idx <- factor(as.character(ks.sizes$window_idx), levels = wID)
+# Import and Subset Data --------------------------------------------------
 
-pID <- (c(unique(ks.sizes$project_id)))
-ks.sizes$project_id <- as.character(ks.sizes$project_id)
+## set working directory
+setwd("~/Documents/GitHub/Vasilescu-diversity/GHDiversityData")
 
-ks.sizes$project_id_f <- factor(ks.sizes$project_id, levels = pID)
+## import main data file
+myDF <- fread("user_data.csv")
 
-ks.sizes$num_team <- as.numeric(as.character(ks.sizes$num_team))
-ks.sizes$github_tenures <- as.numeric(as.character(ks.sizes$github_tenures))
-ks.sizes$commits <- as.numeric(as.character(ks.sizes$commits))
+## import information needed to select projects
+## for modeling
+wmxnList <- fread("wmxn_user_data.csv")
 
-ks.sizes$gh_tenure_scaled <- scale(ks.sizes$github_tenures)[, 1]
-ks.sizes$gh_tenure_log <- log(ks.sizes$github_tenures)
+wmxnList <- unique(wmxnList$project_id)
 
+mxnList <- fread("NoWomanTeams_SmartSampled.csv")
 
-# Subset by Team Size -----------------------------------------------------
+mxnList <- mxnList$project_id
 
-ks.small <- (ks.sizes %>%
-               subset(sizeClass == "small")
-             ) %>%
-  group_by(project_id, window_idx) %>%
-  mutate(Gini_gh_ten = Gini(github_tenures)) %>%
-  ungroup
+genStatus <- fread("project_data_smartSamplingFeatures.csv") %>%
+  subset(select = c("project_id", "hasWomenEver")) %>%
+  distinct(project_id, .keep_all = TRUE)
 
-ks.med <- (ks.sizes %>%
-             subset(sizeClass == "medium")
-           ) %>%
-  group_by(project_id, window_idx) %>%
-  mutate(Gini_gh_ten = Gini(github_tenures)) %>%
-  ungroup
+## select desired projects from main data set
+myDF <- myDF %>%
+  subset(project_id %in% wmxnList | project_id %in%mxnList)
 
-ks.large <- (ks.sizes %>%
-               subset(sizeClass == "large")
-             ) %>%
-  group_by(project_id, window_idx) %>%
-  mutate(Gini_gh_ten = Gini(github_tenures)) %>%
-  ungroup
+## add column to indicate teams with women and no women teams
+myDF <- left_join(myDF, 
+                  genStatus)
 
 
+# Classify Language (Paradigm) -------------------------------------------------------
 
-# Inspect Relationships ---------------------------------------------------
+## based on Ray et al. (2017) http://dx.doi.org/10.1145/3126905
 
-tapply.shingle <- function(x,s,fn,...) {
-  result <- c()
-  for(l in levels(s)) { 
-    x1 <- x[s > l[1] & s < l[2]]
-    result <- c(result, fn(x1,...))
-  }
-  result
-  }
-logit <- function(x) {
-  log(x/(1-x))
-}
+eventDr <- ("Visual Basic")
+declar <- c("ASP", "Puppet", "XSLT")
+impProc <- c("C", "C++", "C#", "Objective-C", "Java", "Go", "Vala")
+impScri <- c("CoffeeScript", "JavaScript", "Python", "Groovy", "Perl", "PHP", "Ruby", "Smalltalk", "TypeScript")
+Funct <- c("Clojure", "Erlang", "Haskell", "Scala", "Emacs Lisp", "Elixir")
+Scri <- c("Shell", "VimL", "Augeas", "PowerShell")
+multiPa <- c("JavaScript", "Lua", "FORTRAN", "R", "ActionScript", "Common Lisp", "Standard ML", "Rust", "Racket", "Matlab", "OCaml", "Julia")
+markup <- c("CSS", "AUGEUS", "XML")
 
-my.intervals <- cbind(1:29-0.5,1:29+1.5)
+myDF <- myDF %>%
+  mutate(langParadigm = ifelse(language %in% impProc, "imperativePocedural",
+                               ifelse(language %in% impScri, "imperativeScripting",
+                                      ifelse(language %in% Funct, "functional",
+                                             ifelse(language %in% multiPa, "multiParadigm",
+                                                    ifelse(language %in% Scri, "script",
+                                                           ifelse(language %in% markup, "markup",
+                                                                  ifelse(language %in% declar, "declarative",
+                                                                         ifelse(language %in% eventDr, "eventDriven",
+                                             "HELP")))))))))
 
-smallResponse <- ks.small$leavesNextQ
-medResponse <- ks.med$leavesNextQ
-largeResponse <- ks.large$leavesNextQ
+TEST <- myDF %>%
+  distinct(project_id, .keep_all = T)
 
-ks.small$Gen <- ifelse(ks.small$gender=="female",1,0)
-ks.med$Gen <- ifelse(ks.med$gender=="female",1,0)
-ks.large$Gen <- ifelse(ks.large$gender=="female",1,0)
+# OLDPrepare Data ------------------------------------------------------------
+
+#ks.sizes <- (kitchen.sink %>%
+#               subset(gender != "None") %>%
+#               mutate(sizeClass=
+#                        ifelse(num_team < 11, "small", 
+#                               ifelse(num_team > 10 & num_team < 30, "medium", 
+#                                      "large"
+#                               ))))
+#
+#
+#wID <- sort(c(unique(ks.sizes$windows), 1))
+#ks.sizes$window_idx <- factor(as.character(ks.sizes$window_idx), levels = wID)
+#
+#pID <- (c(unique(ks.sizes$project_id)))
+#ks.sizes$project_id <- as.character(ks.sizes$project_id)
+#
+#ks.sizes$project_id_f <- factor(ks.sizes$project_id, levels = pID)
+#
+#ks.sizes$num_team <- as.numeric(as.character(ks.sizes$num_team))
+#ks.sizes$github_tenures <- as.numeric(as.character(ks.sizes$github_tenures))
+#ks.sizes$commits <- as.numeric(as.character(ks.sizes$commits))
+#
+#ks.sizes$gh_tenure_scaled <- scale(ks.sizes$github_tenures)[, 1]
+#ks.sizes$gh_tenure_log <- log(ks.sizes$github_tenures)
+#
+#
+# OLDSubset by Team Size -----------------------------------------------------
+
+#ks.small <- (ks.sizes %>%
+#               subset(sizeClass == "small")
+#             ) %>%
+#  group_by(project_id, window_idx) %>%
+#  mutate(Gini_gh_ten = Gini(github_tenures)) %>%
+#  ungroup
+#
+#ks.med <- (ks.sizes %>%
+#             subset(sizeClass == "medium")
+#           ) %>%
+#  group_by(project_id, window_idx) %>%
+#  mutate(Gini_gh_ten = Gini(github_tenures)) %>%
+#  ungroup
+#
+#ks.large <- (ks.sizes %>%
+#               subset(sizeClass == "large")
+#             ) %>%
+#  group_by(project_id, window_idx) %>%
+#  mutate(Gini_gh_ten = Gini(github_tenures)) %>%
+#  ungroup
+#
 
 
-size.x <- with(ks.large,tapply.shingle(num_team,
-                                      shingle(num_team,my.intervals),mean))
-size.y <- with(ks.large,tapply.shingle(largeResponse,
-                                      shingle(num_team,my.intervals),mean))
-plot(size.x,logit(size.y))
+# OLDInspect Relationships ---------------------------------------------------
 
-ght.x <- with(ks.large,tapply.shingle(github_tenures,
-                                          shingle(github_tenures,my.intervals),mean))
-ght.y <- with(ks.large,tapply.shingle(largeResponse,
-                                            shingle(github_tenures,my.intervals),mean))
-plot(ght.x,logit(ght.y))
-#plot(ght.x,ght.y)
+#tapply.shingle <- function(x,s,fn,...) {
+#  result <- c()
+#  for(l in levels(s)) { 
+#    x1 <- x[s > l[1] & s < l[2]]
+#    result <- c(result, fn(x1,...))
+#  }
+#  result
+#  }
+#logit <- function(x) {
+#  log(x/(1-x))
+#}
+#
+#my.intervals <- cbind(1:29-0.5,1:29+1.5)
+#
+#smallResponse <- ks.small$leavesNextQ
+#medResponse <- ks.med$leavesNextQ
+#largeResponse <- ks.large$leavesNextQ
+#
+#ks.small$Gen <- ifelse(ks.small$gender=="female",1,0)
+#ks.med$Gen <- ifelse(ks.med$gender=="female",1,0)
+#ks.large$Gen <- ifelse(ks.large$gender=="female",1,0)
+#
+#
+#size.x <- with(ks.large,tapply.shingle(num_team,
+#                                      shingle(num_team,my.intervals),mean))
+#size.y <- with(ks.large,tapply.shingle(largeResponse,
+#                                      shingle(num_team,my.intervals),mean))
+#plot(size.x,logit(size.y))
+#
+#ght.x <- with(ks.large,tapply.shingle(github_tenures,
+#                                          shingle(github_tenures,my.intervals),mean))
+#ght.y <- with(ks.large,tapply.shingle(largeResponse,
+#                                            shingle(github_tenures,my.intervals),mean))
+#plot(ght.x,logit(ght.y))
+##plot(ght.x,ght.y)
+#
+#gen.y <- with(ks.large,tapply.shingle(largeResponse,
+#                                      shingle(Gen,my.intervals),mean))
+#gen.x <- with(ks.large,tapply.shingle(Gen,
+#                                        shingle(Gen,my.intervals),mean))
+#plot(gen.x,gen.y)
+#
+#ght.x <- with(ks.large,tapply.shingle(github_tenures,
+#                                      shingle(Gini_gh_ten,my.intervals),mean))
+#ght.y <- with(ks.large,tapply.shingle(largeResponse,
+#                                      shingle(Gini_gh_ten,my.intervals),mean))
+#plot(ght.x,logit(ght.y))
+##plot(ght.x,ght.y)
+#
+#gen.y <- with(ks.large,tapply.shingle(largeResponse,
+#                                      shingle(blau_gender,my.intervals),mean))
+#gen.x <- with(ks.large,tapply.shingle(Gen,
+#                                      shingle(blau_gender,my.intervals),mean))
+#plot(gen.x,logit(gen.y))
+#
 
-gen.y <- with(ks.large,tapply.shingle(largeResponse,
-                                      shingle(Gen,my.intervals),mean))
-gen.x <- with(ks.large,tapply.shingle(Gen,
-                                        shingle(Gen,my.intervals),mean))
-plot(gen.x,gen.y)
 
-ght.x <- with(ks.large,tapply.shingle(github_tenures,
-                                      shingle(Gini_gh_ten,my.intervals),mean))
-ght.y <- with(ks.large,tapply.shingle(largeResponse,
-                                      shingle(Gini_gh_ten,my.intervals),mean))
-plot(ght.x,logit(ght.y))
-#plot(ght.x,ght.y)
+# Plot Distributions ------------------------------------------------------
 
-gen.y <- with(ks.large,tapply.shingle(largeResponse,
-                                      shingle(blau_gender,my.intervals),mean))
-gen.x <- with(ks.large,tapply.shingle(Gen,
-                                      shingle(blau_gender,my.intervals),mean))
-plot(gen.x,logit(gen.y))
+## user characteristics
+ggplot(myDF,
+       aes(x = gender)) +
+  geom_bar(stat = "count")
+
+ggplot(myDF,
+       aes(x = github_tenure)) +
+  geom_histogram() ## kind of normal but not really
+
+
+ggplot(myDF,
+       aes(x = commits)) +
+  geom_histogram() ## heavy skew
+
+## group composition
+ggplot(myDF,
+       aes(x = blau_gender)) +
+  geom_histogram() ## heavy skew
+
+ggplot(myDF,
+       aes(x = Gini_gh_ten)) + 
+  geom_histogram() ## nearly normal, some skew
+
+## project characteristics
+
+wmCheck <- myDF %>%
+  distinct(project_id, .keep_all =TRUE)
+
+ggplot(wmCheck,
+       aes(x = hasWomenEver)) +
+  geom_bar(stat = "count")
+
+ggplot(myDF,
+       aes(x = num_team)) + 
+  geom_histogram() ## heavy skew
+
+###not sure it makes sense to include these given it's the same num at each timepoint?
+ggplot(myDF,
+       aes(x = forks)) +
+  geom_histogram() ## heavy skew
+
+ggplot(myDF,
+       aes(x = watchers)) +
+  geom_histogram() ## heavy skew
 
 # Simple Correlations ------------------------------------------------------------
 
-ggpairs(ks.small[, c("gender", "gh_tenure_scaled", "blau_gender", "Gini_gh_ten")])
+ggpairs(myDF[, c("gender", 
+                 "github_tenure", 
+                 "blau_gender", 
+                 "Gini_gh_ten")])
 
 ggpairs(ks.med[, c("gender", "gh_tenure_scaled", "blau_gender", "Gini_gh_ten")])
 
